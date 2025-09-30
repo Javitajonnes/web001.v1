7d0943161d647d231109e0c12755a0f7e02c0955
Javitajonnes <javitajonnes@gmail.com>
2025-09-30 00:44:00 -0300

    css

--- posteo/models.py
+++ posteo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Noticia(models.Model):
     titulo=models.CharField(max_length=100)
     detalle=models.TextField()
-    imagen=models.ImageField()
+    imagen=models.ImageField(upload_to='noticias/', blank=True, null=True)
 
     def __str__(self):
         return self.titulo
--- web001/urls.py
+++ web001/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from core import views as views_core
 from posteo import views as views_posteo
 
@@ -10,3 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ]
+
+# Servir archivos media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
