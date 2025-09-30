d152a28875c15bde977ecaf794a91dacd8b0c09c
Javitajonnes <javitajonnes@gmail.com>
2025-09-30 00:22:39 -0300

    reparando noticias

--- posteo/models.py
+++ posteo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Noticia(models.Model):
     titulo=models.CharField(max_length=100)
     detalle=models.TextField()
+    imagen=models.ImageField()
 
     def __str__(self):
         return self.titulo
@@ -10,7 +11,7 @@ class Noticia(models.Model):
 class Replica(models.Model):
     name=models.CharField(max_length=50)
     detalle=models.TextField()
-    numer=models.SmallIntegerField()
+    numero=models.SmallIntegerField()
 
     def __str__(self):
             return self.name
--- posteo/views.py
+++ posteo/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Noticia
 
 def noticia(request):
-    noticia=Noticia.objects.all() # devuelve lista de noticias desde BD
-    return render(request,"posteo/noticias.html",{"noticia":noticia})
+    noticias=Noticia.objects.all() # devuelve lista de noticias desde BD
+    return render(request,"posteo/noticias.html",{"noticias":noticias})
